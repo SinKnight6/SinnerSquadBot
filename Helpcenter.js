cf1f257c2b574af14f393ca4465631a0dd1c5702
SinKnight6 <sinnersquadhq@gmail.com>
2020-08-24 22:47:05 -0500

    fourth commit

--- Helpcenter.js
+++ Helpcenter.js
@@ -106,7 +106,7 @@ Disconnects the bot from your voice channel and clears the queue.
          .catch(() => {
           let uEmbed = new Discord.MessageEmbed()         
           .setColor(0xFF0000)
-          .setTitle(`**${message.author}**, Your help request was cancelled`)
+          .setDescription(`**${message.author}**, Your help request was cancelled`)
           .addField('**Error**','**You did not say any commands within the time limit!, to summon me again please use,  ``!live chat`` command.**');
            message.channel.send({embed: uEmbed});
          });
