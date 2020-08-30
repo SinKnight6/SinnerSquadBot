3f5130bb00c02976110dbe5645959f4b5de4013f
SinKnight6 <sinnersquadhq@gmail.com>
2020-08-30 16:56:32 -0500

    sixth commit

--- LiveChat.js
+++ LiveChat.js
@@ -490,9 +490,7 @@ const embed = new Discord.MessageEmbed()
       const forwardsFilter = (reaction, user) => reaction.emoji.name === ('➡️') && user.id === message.author.id;
 
       const backwards = msg.createReactionCollector(backwardsFilter, { time: 5400000 });
-      reaction.users.remove(user);
       const forwards = msg.createReactionCollector(forwardsFilter, { time: 5400000 });
-      reaction.users.remove(user);
 
       backwards.on('collect', r => {
         if (page === 1) return;
